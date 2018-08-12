18f5771b2cc8eadfb1afcd7bcba9c866181ddf7b
James Lu <jameslu@Jamesde-MacBook-Pro.local>
2018-08-12 15:38:30 +0800

    add supertest, modify test case

--- test/index.text.js
+++ test/index.text.js
@@ -1,8 +1,19 @@
 const should = require('should')
+const supertest = require('supertest')
 require('../index.js')
+
+const api = supertest('http://localhost:1234')
 
 describe('Simple http server test', function() {
   it('should return', function(done) {
-    done() 
+    api.get('/')
+      .expect(200)
+      .end((err, res) => {
+        if (err) { return done(err) }
+        
+        res.body.should.be.Object()
+        res.text.should.equal('hello')
+        done()
+      })
   })
 })
